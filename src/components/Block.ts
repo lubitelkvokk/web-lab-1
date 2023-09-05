9dde7aee4f72b1cee6fb33fc6ea077654c74afb9
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2023-09-05 23:44:28 +0300

    Structure of date for html tags(name of tag, id, content)

---
+++ src/components/Block.ts
@@ -0,0 +1,24 @@
+import Parameters from "./Parameters";
+import {stylesheetToCSSStylesConvertation} from "../utils/StylesheetToCSSStyles";
+
+
+export default class Block{
+    private tag: string;
+    private id: string;
+    private content: string;
+
+    constructor(tag:string, id: string, content: string) {
+        this.tag = tag;
+        this.id = id;
+        this.content = content;
+    }
+
+    toHTML() {
+        return `<${this.tag}
+        ${this.id ? `id="${this.id}"` : ""}> 
+        ${this.content}
+        </${this.tag}>`;
+    }
+
+
+}

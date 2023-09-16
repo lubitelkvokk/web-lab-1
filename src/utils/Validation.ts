cc5b45fb472339aeb9d3f9553afb62f92ca09fc1
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2023-09-16 21:35:49 +0300

    added the ability to transfer elements

--- src/utils/Validation.ts
+++ src/utils/Validation.ts
@@ -1,59 +1,44 @@
-export default function validation() {
-    const xInputs: NodeListOf<HTMLInputElement> | null = document.querySelectorAll('input[name="xChoice"]');
-    const yInput: HTMLInputElement | null = document.querySelector('input[name="yChoice"]');
-    const rInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="rChoice"]');
-    const xError = document.querySelector('#x-error');
-    const yError = document.querySelector('#y-error');
-    const rError = document.querySelector('#r-error');
+export default function validation(x: HTMLInputElement | null, y: HTMLInputElement | null, r: HTMLInputElement | null,
+                                   xError: Element | null, yError: Element | null, rError: Element | null) {
 
-    var isOk = true;
-
-
-    var xChecking = false;
-    xInputs.forEach((element) => {
-        if (element.classList.contains("active")) {
-            if (xError !== null) {
-                xChecking = true;
-                xError.textContent = "";
-            }
-        }
-    })
-    if (!xChecking) {
-        if (xError !== null) {
-            isOk = false;
-            xError.textContent = "Значение поля X должно быть выбрано";
-        }
-    }
-    if (yInput != null) {
-        const y = parseFloat(yInput.value);
-        if (y == null || y <= -5 || y >= 3) {
-            if (yError != null) {
-                yError.textContent = "-5 < Y < 3";
-                isOk = false;
-            }
-        } else {
-            if (yError != null) {
-                yError.textContent = "";
-            }
-        }
+    let isVaild: boolean = true;
+    if (!xError || !yError || !rError) {
+        console.error("Some fields for errors not exists or access")
+        isVaild = false;
     }
 
-
-    var rChecking = false;
-    rInputs.forEach((element) => {
-        if (element.classList.contains("active")) {
-            if (rError !== null) {
-                rChecking = true;
-                rError.textContent = "";
-            }
-        }
-    })
-    if (!rChecking) {
-        if (rError !== null) {
-            isOk = false;
-            rError.textContent = "Значение поля R должно быть выбрано";
-        }
+    // Проверка X
+    if (x?.value == "") {
+        xError!.textContent = "Значение поля X должно быть выбрано";
+        isVaild
+    } else {
+        xError!.textContent = "";
     }
-    return isOk;
+
+    // Проверка y
+    if (y!.value == "" || y == null) {
+        yError!.textContent = "Значение поля Y должно быть выбрано";
+        isVaild = false;
+    } else {
+        let y_number = parseFloat(y!.value);
+        if (y_number <= -5 || y_number >= 3) {
+            yError!.textContent = "-5 < Y < 3";
+            isVaild = false;
+        }
+        else {
+            yError!.textContent = "";
+        }
+
+    }
+
+    // Проверка R
+    if (r?.value == "") {
+        rError!.textContent = "Значение поля R должно быть выбрано";
+        isVaild = false;
+    } else {
+        rError!.textContent = "";
+    }
+
+    return isVaild;
 
 }
